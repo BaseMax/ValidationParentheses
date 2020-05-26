7759b9087a3ec6d54ac2bb3da714f852e58a93a7
Max Base <MaxBaseCode@Gmail.Com>
2020-05-27 01:57:15 +0430

    Update main.c
    Base of array from: https://github.com/mafintosh/array.c

--- main.c
+++ main.c
@@ -3,3 +3,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #define DEFAULT_SIZE 64
+typedef enum {
+	false,
+	true,
+} bool;
+typedef struct {
+	int *buffer;
+	int refs;
+	size_t buffer_size;
+	size_t length;
+} array;
